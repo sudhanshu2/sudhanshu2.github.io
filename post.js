10ec4c5f2904f94581933856d4e1465b8ea5b534
Sudhanshu Agarwal <sudhanshuagarwal2002@gmail.com>
2017-09-12 18:43:59 +0530

    Update post.js

--- post.js
+++ post.js
@@ -3,12 +3,12 @@ var postobj = [
       date: '30 July 2017',
       number: 1,
       title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-        
+
       para1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis, sapien a egestas aliquet, erat ipsum tempus lectus, sed sollicitudin elit dui ac odio. Nam sit amet lacus scelerisque, iaculis dolor et, tempor leo. Fusce scelerisque eget augue et consectetur. Cras vitae ante consectetur, luctus neque eget, rutrum ante. Curabitur cursus lorem diam, quis tincidunt nulla luctus sed. Aliquam consequat a erat vel lobortis. Maecenas mattis eget odio in dignissim. Duis nec mauris viverra, tristique nunc molestie, sodales elit.',
       para2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis, sapien a egestas aliquet, erat ipsum tempus lectus, sed sollicitudin elit dui ac odio. Nam sit amet lacus scelerisque, iaculis dolor et, tempor leo. Fusce scelerisque eget augue et consectetur. Cras vitae ante consectetur, luctus neque eget, rutrum ante. Curabitur cursus lorem diam, quis tincidunt nulla luctus sed. Aliquam consequat a erat vel lobortis. Maecenas mattis eget odio in dignissim. Duis nec mauris viverra, tristique nunc molestie, sodales elit.',
       para3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis, sapien a egestas aliquet, erat ipsum tempus lectus, sed sollicitudin elit dui ac odio. Nam sit amet lacus scelerisque, iaculis dolor et, tempor leo. Fusce scelerisque eget augue et consectetur. Cras vitae ante consectetur, luctus neque eget, rutrum ante. Curabitur cursus lorem diam, quis tincidunt nulla luctus sed. Aliquam consequat a erat vel lobortis. Maecenas mattis eget odio in dignissim. Duis nec mauris viverra, tristique nunc molestie, sodales elit.',
       para4: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sagittis, sapien a egestas aliquet, erat ipsum tempus lectus, sed sollicitudin elit dui ac odio. Nam sit amet lacus scelerisque, iaculis dolor et, tempor leo. Fusce scelerisque eget augue et consectetur. Cras vitae ante consectetur, luctus neque eget, rutrum ante. Curabitur cursus lorem diam, quis tincidunt nulla luctus sed. Aliquam consequat a erat vel lobortis. Maecenas mattis eget odio in dignissim. Duis nec mauris viverra, tristique nunc molestie, sodales elit.',
-        
+
       coverphoto: 'img1cover',
       coverphototxt: 'Tamalpais-Homestead Valley, CA &copy; Sudhanshu Agarwal',
       photo1: 'noshow',
@@ -24,12 +24,12 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '6 August 2017',
       number: 2,
       title: 'Hello World!',
-        
+
       para1: 'Hello World! I am starting a blog designed using Materialize CSS and hosted on GitHub pages. I will usually post random or science stuff. I will usually update it every fornight. I will start it by writing about my Google Code-in 2016 experience.',
       para2: 'I am a big fan of simplicity and material design and you can see that in this website.',
       para3: 'Starting with a little about me, I am Sudhanshu Agarwal and Hi, it is great to meet you! I am a quizzer and a programmer and also I am deep into music.I was a Grand Prize Winner with BRL-CAD in Google Code-in 2016 but watch out for my next post for more details.I hope that you will enjoy reading the rest of the posts and please feel free to contact me via the contact me page whose link is floating in the bottom left of the website.',
@@ -49,12 +49,12 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '13 August 2017',
       number: 3,
       title: 'My Google Code-in 2016 Experience',
-        
+
       para1: 'Post coming up!',
       para2: '',
       para3: '',
@@ -74,12 +74,12 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '20 August 2017',
       number: 4,
       title: 'Adios LA',
-        
+
       para1: 'Post coming up!',
       para2: '',
       para3: '',
@@ -99,18 +99,18 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '27 August 2017',
       number: 5,
-      title: 'BRL-CAD 101',
-        
+      title: 'History of BRL-CAD<',
+
       para1: 'Post coming up!',
       para2: '',
       para3: '',
       para4: '',
       coverphoto: 'img5cover',
-      coverphototxt: 'New Delhi, IN &copy; Delhi Public School, R. K. Puram',
+      coverphototxt: 'BRL&#45;CAL Logo &copy; BRL&#45;CAL',
       photo1: 'noshow',
       photo1txt: '',
       photo2: 'noshow',
@@ -124,12 +124,12 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '3 September 2017',
       number: 6,
       title: 'Google Code-in 2016 Grand Prize Winner&#39;s Trip',
-        
+
       para1: 'Post coming up!',
       para2: '',
       para3: '',
@@ -149,12 +149,12 @@ var postobj = [
       code3: 'noshow',
       code4: 'noshow',
     },
-    
+
     {
       date: '10 September 2017',
       number: 7,
       title: '2017: A Quizzer&#39;s Review',
-        
+
       para1: 'Post coming up!',
       para2: '',
       para3: '',
